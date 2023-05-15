9a21b9818902be04b2b9f3b5dd098ee2a9c5cdda
myantyuWorld <nepinepi984@gmail.com>
2023-05-15 13:09:25 +0900

    UI Modify

--- src/components/MemoList.tsx
+++ src/components/MemoList.tsx
@@ -9,7 +9,10 @@ export default function MemoList(props: any) {
     <Card>
       <CardContent>
         <Typography mb={1}>memo list</Typography>
-        <Box sx={{ flexGrow: 1, overflow: 'hidden', px: 0 }}>
+        <Box sx={{ flexGrow: 1, overflow: 'hidden', px: 0, 
+          flexDirection: "column",
+          height: 700,
+          overflowY: "scroll", }}>
           {props.data.map((item: any, index: number) => (
             <MemoItem key={index} data={item} handleDeleteMemo={() => handleDeleteMemo(item.mmid)}></MemoItem>
           ))}
