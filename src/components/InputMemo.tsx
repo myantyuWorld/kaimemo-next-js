61524f799cc9a9dfcf9dab2107e7062c8eab8a6d
myantyuWorld <nepinepi984@gmail.com>
2023-05-29 14:49:22 +0900

    useEffect

--- src/components/InputMemo.tsx
+++ src/components/InputMemo.tsx
@@ -6,6 +6,17 @@ import SoapIcon from '@mui/icons-material/Soap';
 export default function InputMemo(props: { handleRegistMemo: (arg0: { mmsb: string; mmnm: string; }) => void; }) {
   const [alignment, setAlignment] = React.useState('food');
   const [description, setDescription] = React.useState("")
+  const [message, setMessage] = React.useState("買い物メモを入力してね。")
+
+  // useEffectに渡された関数は、レンダーの結果が画面の反映された「あと」に動作する。
+  // つまり、「関数の実行タイミングをReactのレンダリング後まで遅らせるHook」
+  // 
+  //　副作用処理（DOM書き換え、変数代入、API通信などUI構築以外の処理）を関数コンポーネントで扱えるようになる。
+  // 第1引数 : 副作用処理
+  // 第2引数 : 副作用処理の、実行タイミングを制御する依存データ
+  React.useEffect(() => {
+    setMessage(description == "" ? "買い物メモを入力してね。" : "入力漏れはないですか？")
+  }, [description])
 
   const handleChange = (
     event: React.MouseEvent<HTMLElement>,
@@ -27,6 +38,15 @@ export default function InputMemo(props: { handleRegistMemo: (arg0: { mmsb: stri
         <Grid item xs={12}>
           <Card>
             <CardContent>
+              <Stack
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+                divider={<Divider orientation="vertical" flexItem />}
+                spacing={1}
+                mb={2}>
+                  {message}
+                </Stack>
               <Stack
                 direction="row"
                 justifyContent="center"
