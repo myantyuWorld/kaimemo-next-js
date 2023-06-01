5dd0eaa1ae96f1e78a7735fc561982924c5515eb
myantyuWorld <nepinepi984@gmail.com>
2023-06-01 18:53:55 +0900

    不要な処理、useSWRをカスタムフック化

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,34 +4,20 @@ import { Box, Button, Container, Fab, Typography } from '@mui/material';
 
 import CalculateIcon from '@mui/icons-material/Calculate';
 import CameraAltIcon from '@mui/icons-material/CameraAlt';
-import QueryStatsIcon from '@mui/icons-material/QueryStats';
-import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
 import InputMemo from '../features/memo/components/InputMemo';
 import MemoList from '../features/memo/components/MemoList';
 import useSWR from 'swr'
 import FilterMemo from '../features/memo/components/FilterMemo';
-import CalculateButton from '../features/calculator/components/CaluclationButton';
-import LinearProgress from '@mui/material/LinearProgress';
-import RecognitionButton from '../features/recognition/components/RecognitionButton';
 import RecognitionResultButton from '../features/recognition/components/RecognitionResultButton';
 import BaseButton from '../components/elements/Button/BaseButton';
-
-
+import { useFetchMemo } from '../hooks/useFetchData';
 
 
 export default function Home() {
-  // https://zenn.dev/uttk/articles/b3bcbedbc1fd00
-  const fetcher = (url: string): Promise<any> => fetch(url).then(res => res.json());
-
-  // http://os3-357-11662.vs.sakura.ne.jp:18083/rakuzaim08/vue/memo/index
-  // [{"mmid":1183,"mmsb":1,"mmnm":"袋とじ","count":0,"deleteFlg":0}]
   const API_URL = "https://fby1jt4nzc.execute-api.ap-northeast-1.amazonaws.com/Prod/memo"
-  // const { data, error } = useSWR('https://fby1jt4nzc.execute-api.ap-northeast-1.amazonaws.com/Prod/memo', fetcher)
-  const { data, error } = useSWR(API_URL, fetcher, { refreshInterval: 7000 })
-
-  const postData = (data: { mmsb: string; mmnm: string; }) => {
+  const handleRegistMemo = (data: { mmsb: string; mmnm: string; }) => {
     // fetch('https://fby1jt4nzc.execute-api.ap-northeast-1.amazonaws.com/Prod/memo', {
     fetch(API_URL, {
       mode: "cors",
@@ -43,32 +29,10 @@ export default function Home() {
     });
   }
 
-  const handleRegistMemo = (data: { mmsb: string; mmnm: string; }) => {
-    postData(data)
-  }
-
   const [filterCategory, setFilterCategory] = React.useState('food');
   const handleFilterChange = (newAlignment: string) => {
     setFilterCategory(newAlignment);
   };
-  const [progress, setProgress] = React.useState(0);
-
-  React.useEffect(() => {
-    const timer = setInterval(() => {
-      setProgress((oldProgress) => {
-        if (oldProgress === 100) {
-          return 0;
-        }
-        const diff = Math.random() * 10;
-        return Math.min(oldProgress + diff, 100);
-      });
-    }, 500);
-
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
-
   // 子→親
   // https://qiita.com/aliceroot0678/items/e4eabcbe3f9f79cada55
   const handleDeleteMemo = (mmid: number) => {
@@ -83,16 +47,15 @@ export default function Home() {
     });
   }
 
-
+  const {data, error} = useFetchMemo()
   if (error) return <div>failed to load</div>
   if (!data) {
     return (
       <Box>
-        <LinearProgress variant="determinate" value={progress} />
+        now loading...
       </Box>
     )
   }
-
   const containerStyle = {
     background: "#f3d2c1"
   }
