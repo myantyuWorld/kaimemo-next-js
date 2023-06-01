693ffa43ebf985b231f1792f75f9155ccb10ed1d
myantyuWorld <nepinepi984@gmail.com>
2023-06-01 14:44:24 +0900

    値札撮影画面　解析結果を画面表示（不要なものまで、S3に保存する必要はないため）

--- src/pages/recognition.tsx
+++ src/pages/recognition.tsx
@@ -1,10 +1,13 @@
-import { Container, Grid, Typography } from "@mui/material";
+import { DeleteForever } from "@mui/icons-material";
+import { Button, Card, CardContent, Container, Grid, Skeleton, Typography } from "@mui/material";
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import Head from "next/head";
 import Link from "next/link";
 import React from "react";
 
 import Webcam from "react-webcam";
-import { createWorker } from 'tesseract.js';
+import Tesseract from "tesseract.js";
+import { createWorker, Worker } from 'tesseract.js';
 
 export default function Recognition() {
   const API_URL = "https://fby1jt4nzc.execute-api.ap-northeast-1.amazonaws.com/Prod/recognition"
@@ -18,22 +21,20 @@ export default function Recognition() {
     height: 720,
     facingMode: "environment"
   };
-  const worker = createWorker({
-    logger: m => console.log(m)
-  })
   const capture = React.useCallback(
     async () => {
       const imageSrc = webcamRef.current?.getScreenshot();
       if (imageSrc) {
         setBase64Img(imageSrc)
 
-        setTextOcr('Recognizing...')
+        setTextOcr('')
         // TODO : bug
-        // await tryOcr();
+        tryOcr(imageSrc)
       }
     },
     [webcamRef]
   );
+
   const recognition = React.useCallback(
     () => {
       console.log("撮影した画像を、解析に回す")
@@ -43,48 +44,91 @@ export default function Recognition() {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ img : base64Img }),
+        body: JSON.stringify({ img: base64Img }),
       });
     },
     [base64Img]
   );
-  const tryOcr = async() => {
-    await worker.load();
-    await worker.loadLanguage('eng');
-    await worker.initialize('eng');
-    const { data: { text } } = await worker.recognize(file);
-    setTextOcr(text);
-    await worker.terminate();
+  const tryOcr = async (buffer: string) => {
+    const worker: Promise<Worker> = createWorker({
+      logger: m => console.log(m)
+    })
+    await (await worker).load();
+    await (await worker).loadLanguage('eng');
+    await (await worker).initialize('eng');
+    await (await worker).setParameters({
+      tessedit_ocr_engine_mode: Tesseract.OEM.TESSERACT_ONLY,
+      tessedit_char_whitelist: '0123456789',
+      tessedit_pageseg_mode: Tesseract.PSM.SINGLE_WORD
+    });
+    const { data: { text } } = await (await worker).recognize(buffer);
+    await (await worker).terminate();
+    setTextOcr(text)
+
+  }
+  const onClickDelete = () => {
+    setBase64Img(null)
+    setTextOcr("")
+  }
+  const containerStyle = {
+    background: "#f3d2c1"
+  }
+  const titleStyle = {
+    color: "#001858"
   }
   return (
     <>
       <Head>
         <title>Kaimemo!</title>
       </Head>
-      <Typography variant='h5' mt={1}><Link href="/">Kaimemo</Link></Typography>
-      <Grid container spacing={0} mt={1}>
-        <Grid item xs={12}>
-          <Webcam
-            audio={false}
-            ref={webcamRef}
-            screenshotFormat="image/jpeg"
-            height={300}
-            width={390}
-            videoConstraints={videoConstraints}
-          />
+      <Container maxWidth="md" sx={containerStyle}>
+        <Typography variant='h3' sx={titleStyle}><Link href="/">Kaimemo!</Link></Typography>
+        <Grid container spacing={0} mt={1}>
+          <Grid item xs={12}>
+            <Card>
+              <CardContent>
+                <Typography variant='h6' sx={titleStyle} mb={1}>プレビュー</Typography>
+                <Webcam
+                  audio={false}
+                  ref={webcamRef}
+                  screenshotFormat="image/jpeg"
+                  width={300}
+                  videoConstraints={videoConstraints}
+                />
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={12} mt={1}>
+            <Card>
+              <CardContent>
+                <Typography variant='h6' sx={titleStyle} mb={1}>解析結果数字</Typography>
+                {
+                  textOcr === "" ? <Skeleton animation="wave"/> : textOcr
+                }
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={12} mt={1}>
+            <Card>
+              <CardContent>
+                <Typography variant='h6' sx={titleStyle} mb={1}>スクリーンショット</Typography>
+                {
+                  base64Img ? <img src={base64Img} height={100} /> : <Skeleton  animation="wave" height={100}/>
+                }
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={12} mt={1}>
+            <Card>
+              <CardContent>
+                <button onClick={capture}>Capture</button>
+                <Button variant="contained" color="error" onClick={onClickDelete} startIcon={<DeleteForeverIcon/>}></Button>
+                <button onClick={recognition}>recognition</button>
+              </CardContent>
+            </Card>
+          </Grid>
         </Grid>
-        <Grid item xs={12}>
-          {textOcr}
-        </Grid>
-        <Grid item xs={12}>
-          <img src={base64Img} height={300}/>
-        </Grid>
-        <Grid item xs={12}>
-          <button onClick={capture}>Capture photo</button>
-          <button onClick={() => setBase64Img(null)}>delete</button>
-          <button onClick={recognition}>recognition</button>
-        </Grid>
-      </Grid>
+      </Container>
     </>
   )
 }
